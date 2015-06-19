3b185ee70187b606295a545abfc4c2cb6da3c2b6
Terry Harmer <terry.harmer@gmail.com>
2015-06-18 21:49:30 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+
+  get "movies" => "movies#index"
+ 
+end
---
+++ spec/list_movies_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Viewing the list of movies" do
+	
+  it "shows the movies" do 
+  	visit movies_path
+
+ 	expect(page).to have_text("3 Movies")
+ 	expect(page).to have_text("Movie 1")
+ 	expect(page).to have_text("Movie 2")
+ 	expect(page).to have_text("Movie 3")
+  end
+end
